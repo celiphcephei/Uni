219fcade6af169a0d5228e0b53257c23d1b3f930
Xoerix <ampheriz0@gmail.com>
2022-09-06 06:52:54 -0500

    practicas for

--- Primer semestre/Pruebas/for.py
+++ Primer semestre/Pruebas/for.py
@@ -1,3 +1,30 @@
-n = int(input("Escribe un numero: "))
-for i in range(11):
-    print("{} X {} = {}".format(n, i, i * n))
+# def todos_menos_tu(n):
+#     for i in range(1, 101):
+#         if i != n:
+#             print(i)
+
+
+# def cuadrados(n):
+#     for i in range(n, 0, -1):
+#         print(i ** 2)
+
+
+# def pares_nones():
+#     contador_pares = 0
+#     contador_nones = 0
+#     for i in range(10):
+#         i = int(input("Escribe un número: "))
+#         if i % 2 == 0:
+#             contador_pares += 1
+#         else:
+#             contador_nones += 1
+#     print("Hay {} pares y {} nones".format(contador_pares, contador_nones))
+
+
+# n = int(input("Escribe un numero: "))
+# for i in range(11):
+#     print("{} X {} = {}".format(n, i, i * n))
+
+
+def mcd(a, b):
+    pass
