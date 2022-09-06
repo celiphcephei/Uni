b86c79a2efeacf3d609ddbf99dd73cb92bae0d26
Xoerix <ampheriz0@gmail.com>
2022-09-06 07:05:16 -0500

    Proyecto Final

--- Primer semestre/Proyectos_Finales/proyecto1_proyectoIntegrador.py
+++ Primer semestre/Proyectos_Finales/proyecto1_proyectoIntegrador.py
@@ -1,10 +1,30 @@
 def mathMain():
-    pass
+    print("lorem")
+    opc = input("¿Qué quieres hacer?")
+
 
 
 def main():
     print("Proyecto Integrador")
     print("Tecnológico de Monterrey")
+    print("Renato García Morán A01799387")
+    print()
+
+    opcion = ''
+    opcion.lower()
+    iniciador = 0
+
+    while iniciador == 0:
+        opcion = input("¿Qué quieres probar?\nEscribe el nombre del integrante o el nombre del módulo\nMatemáticas (Renato)\nCiencias()\nLectura ()\n")
+        if opcion == "renato" or opcion == "matematicas" or opcion == "matemáticas":
+            print()
+            mathMain()
+            iniciador += 1
+        elif opcion == "ciencias":
+            pass
+
+
+
 
 
 
