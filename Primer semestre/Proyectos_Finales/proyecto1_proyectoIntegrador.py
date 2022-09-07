bf6a6a2f0073fe5f26d5c0b964bd9bb208579302
Xoerix <ampheriz0@gmail.com>
2022-09-06 22:30:12 -0500

    Proyecto Final

--- Primer semestre/Proyectos_Finales/proyecto1_proyectoIntegrador.py
+++ Primer semestre/Proyectos_Finales/proyecto1_proyectoIntegrador.py
@@ -1,22 +1,56 @@
-def mathMain():
-    print("lorem")
-    opc = input("¿Qué quieres hacer?")
+import random
 
+
+def ejercicio_multiplicaciones():
+    print()
+    print("Vamos a ver las multiplicaciones.")
+    inicio1 = random.randint(0, 9)
+    inicio2 = random.randint(0,9)
+    inicio = 0
+    while inicio == 0:
+        iniciador = int(input("Para comenzar, escribe el resultado de {} X {}.\n".format(inicio1, inicio2)))
+        if iniciador == (inicio1 * inicio2):
+            print("hola")
+        else:
+            print("Necesitas ayuda?")
+
+
+
+
+def mathMain():
+    print("Bienvenido al módulo de matemáticas")
+    print("¿Qué quieres ver?\nEscribe el número corresponiente.")
+    iniciador = 0
+    opc = 0
+    while iniciador == 0:
+        opc = int(input("\4 1 Multiplicaciones\n\4 2 Lorem\n\4 3 Lorem"))
+        if opc == 1:
+            ejercicio_multiplicaciones()
+        elif opc == 2:
+            pass
+        elif opc == 3:
+            pass
+        else:
+            print()
+            print("Elige una opción válida")
+            continue
+
+
+    
 
 def ciencias():
-    print("Lorem")
-
+    print("")
 
 
 def main():
     print("Proyecto Integrador")
     print("Tecnológico de Monterrey")
-    print("Renato García Morán A01799387")
+    print("\4 Renato García Morán A01799387\n\4 Andrés Cabrera Alvarado A01798681\n\4 José María A01799580")
     print()
     iniciador = 0
     while iniciador == 0:
-        opcion = input("¿Qué quieres probar?\nEscribe el nombre del integrante o el nombre del módulo\nMatemáticas (Renato)\nCiencias()\nLectura ()\n")
-        opcion.lower()
+        opcion = input("¿Qué quieres probar?\nEscribe el nombre del integrante o el nombre del módulo\nMatemáticas (Renato)\nCiencias(José)\nLectura (Andrés)\n")
+        opcion = opcion.lower()
         if opcion == "renato" or opcion == "matematicas" or opcion == "matemáticas":
             print()
             mathMain()
@@ -28,10 +62,6 @@ def main():
             print()
             print("Elige una opción válida")
             continue
-
-
-
-
 
 
 if __name__ == "__main__":
